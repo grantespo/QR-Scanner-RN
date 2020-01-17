e75a680417fdfe74753dde9d94d2d5ac3ec4d4fe
Grant Espanet <gespanet97@gmail.com>
2020-01-16 21:10:47 -0500

    table-select-from (#4)

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,198 +1,118 @@
-import * as WebBrowser from 'expo-web-browser';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import * as SQLite from 'expo-sqlite';
+
 import {
-  Image,
-  Platform,
-  ScrollView,
+  FlatList,
   StyleSheet,
+  Image,
   Text,
   TouchableOpacity,
   View,
 } from 'react-native';
-
-import { MonoText } from '../components/StyledText';
+import { SafeAreaView } from 'react-navigation';
 
 export default function HomeScreen() {
+  const [qrCodes, setQRCodes] = useState([]);
+  const qrDB = SQLite.openDatabase('qrDB');
+
+  useEffect(() => {
+    (async () => {
+      qrDB.transaction(tx => {
+        tx.executeSql(
+          "create table if not exists items (id integer primary key not null, url text);"
+        );
+        tx.executeSql("select * from items", [], (_, { rows }) => 
+              {
+                var len = rows._array.length;
+                var data = [];
+
+                for (let i = 0; i < len; i++) {
+                  data.push(rows._array[i]);
+                }
+                setQRCodes(data);
+              }
+            );
+      });
+    })();
+  }, []);
+
+   const _renderItem = (item) => {
+    return (
+      <CodeListItem
+        item={item.item}
+      />
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.contentContainer}>
-        <View style={styles.welcomeContainer}>
-          <Image
-            source={
-              __DEV__
-                ? require('../assets/images/robot-dev.png')
-                : require('../assets/images/robot-prod.png')
-            }
-            style={styles.welcomeImage}
-          />
-        </View>
-
-        <View style={styles.getStartedContainer}>
-          <DevelopmentModeNotice />
-
-          <Text style={styles.getStartedText}>Get started by opening</Text>
-
-          <View
-            style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-            <MonoText>screens/HomeScreen.js</MonoText>
-          </View>
-
-          <Text style={styles.getStartedText}>
-            Change this text and your app will automatically reload.
-          </Text>
-        </View>
-
-        <View style={styles.helpContainer}>
-          <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-            <Text style={styles.helpLinkText}>
-              Help, it didn’t automatically reload!
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
-
-      <View style={styles.tabBarInfoContainer}>
-        <Text style={styles.tabBarInfoText}>
-          This is a tab bar. You can edit it in:
-        </Text>
-
-        <View
-          style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-          <MonoText style={styles.codeHighlightText}>
-            navigation/MainTabNavigator.js
-          </MonoText>
-        </View>
-      </View>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+          data={Object.values(qrCodes)} 
+          renderItem={item => _renderItem(item)}
+          /> 
+    </SafeAreaView>
   );
+}
+
+class CodeListItem extends React.PureComponent { 
+
+  render() {
+   return (
+    <TouchableOpacity style={styles.itemContainter}>
+      <View style={styles.idContainer}>
+   <Text style={styles.idText}>{this.props.item.id}</Text>
+      </View>
+      <View style={styles.itemURLContainer}>
+   <Text style={styles.URLText}>{this.props.item.url}</Text>
+      </View>
+      <TouchableOpacity style={styles.itemDeleteContainer}>
+        <Image style={styles.deleteImage}></Image>
+      </TouchableOpacity>
+    </TouchableOpacity>
+    )}
 }
 
 HomeScreen.navigationOptions = {
   header: null,
 };
 
-function DevelopmentModeNotice() {
-  if (__DEV__) {
-    const learnMoreButton = (
-      <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
-        Learn more
-      </Text>
-    );
-
-    return (
-      <Text style={styles.developmentModeText}>
-        Development mode is enabled: your app will be slower but you can use
-        useful development tools. {learnMoreButton}
-      </Text>
-    );
-  } else {
-    return (
-      <Text style={styles.developmentModeText}>
-        You are not in development mode: your app will run at full speed.
-      </Text>
-    );
-  }
-}
-
-function handleLearnMorePress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/development-mode/'
-  );
-}
-
-function handleHelpPress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/up-and-running/#cant-see-your-changes'
-  );
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
   },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
+  itemContainter: {
+    flex: 1, 
+    flexDirection: 'row',
+    backgroundColor: '#D4D4D4',
+    height: 75,
+    marginTop: 3
+  },
+  idContainer: {
+    flex: 0.15,
+    justifyContent: 'center',
+    alignContent: 'center'
+  },
+  idText: {
     textAlign: 'center',
+    fontWeight: '500',
+    fontSize: 22,
   },
-  contentContainer: {
-    paddingTop: 30,
+  itemURLContainer: {
+    flex: 0.7,
+    justifyContent: 'center'
   },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
+  URLText: {
+    fontSize: 15,
   },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
+  itemDeleteContainer: {
+    flex: 0.15,
+    justifyContent: 'center',
+    alignContent: 'center'
   },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
+  deleteImage: {
+    width: 25,
+    height: 25,
+    backgroundColor: 'black',
   },
 });
